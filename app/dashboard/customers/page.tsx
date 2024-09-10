812baa27d632e5cd6bff71921fc3114f3319e19e
blevantovych <levantovychbogdan@gmail.com>
2024-09-10 21:21:12 +0300

    lesson 8

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,6 @@
-export default function Page() {
+import { fetchRevenue } from "@/app/lib/data";
+
+export default async function Page() {
+  await fetchRevenue();
   return <p>Customers Page</p>;
 }
